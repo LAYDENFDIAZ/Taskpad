108a88bb90efeae41e96cc8b7c589c139f8c1c13
Layden Diaz <layden@Laydens-MacBook-Air.local>
2023-12-19 12:11:26 -1000

    setup react project and added tailwindCss

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className='w-1/3 bg-slate-800'>
+            <h1 className='text-xl font-mono font-bold'>NOTES</h1>
+        </div>
+    )
+}
+
+export default Sidebar
